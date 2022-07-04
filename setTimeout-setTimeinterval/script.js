227afa532c03a62af43e870d5bd7873441888504
akmandaviya <abdulmandaviya@gmail.com>
2022-07-04 09:50:42 +0530

    new commit

---
+++ setTimeout-setTimeinterval/script.js
@@ -0,0 +1,37 @@
+// Set timeout:-
+// --->(1)
+const a = "First"
+console.log(a)
+
+const b = "Second"
+setTimeout(()=> {
+  console.log(b)
+},2000)
+
+
+// --->(2)
+function a () {
+    var a1 = 10
+    console.log(a1)
+}
+
+const c = setTimeout(a,2000)
+function stopTimer() {
+    clearTimeout(b) //==> timer clear
+}
+
+
+// Set timeinterval
+const d = "using setTimer Interval"
+console.log(d)
+setInterval(() => {
+    const e = "I'm happy!!"
+    console.log(e)
+}, 1000);
+
+function g () {
+    var g1 = 10
+    console.log(g1)
+}
+
+const f = setInterval(g,1000)
