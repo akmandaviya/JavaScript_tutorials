e169b3c10427d34a7d7bffbaff0f09f5f97c3202
akmandaviya <abdulmandaviya@gmail.com>
2022-07-04 09:19:38 +0530

    new commit

--- Promises in js/1.js
+++ Promises in js/1.js
@@ -1,46 +1,46 @@
-// //By using promise constructor
-// //(1)
-// var myPromise = new Promise(function (resolve, reject) { //syntax of promise constructor
+//By using promise constructor
+//(1)
+var myPromise = new Promise(function (resolve, reject) { //syntax of promise constructor
 
-//     const a = "abdulkarim";
-//     const b = "abdulkarim";
+    const a = "abdulkarim";
+    const b = "abdulkarim";
 
-//     if (a == b) {
-//         resolve();
-//     } else {
-//         reject();
-//     }
-// });
+    if (a == b) {
+        resolve();
+    } else {
+        reject();
+    }
+});
 
-// //promise.then --> accepted/fulfilled
-// //promise.catch --> rejected
-// myPromise.then(function () {
-//     console.log("Promise fulfilled");
-// }).catch(function () {
-//     console.log("Promise rejected");
-// });
-
-
-
-// //(2)
-// //this is producing promise
-// const obj = new Promise((resolve,reject)=>{
-//  setTimeout(() =>{
-//    let rollNo =[1,2,3,4];
-//    resolve(rollNo);
-//    reject('Error while searching');
-//  },2000);
-
-// });
+//promise.then --> accepted/fulfilled
+//promise.catch --> rejected
+myPromise.then(function () {
+    console.log("Promise fulfilled");
+}).catch(function () {
+    console.log("Promise rejected");
+});
 
 
-// //this is consumng promise
-// obj.then((rollNo)=>{
-//     console.log(rollNo); // it will give complete list
-// console.log(rollNo[2]);// roll no  at 2nd index
-// }).catch((error)=> {
-//     console.log(error);
-// });
+
+//(2)
+//this is producing promise
+const obj = new Promise((resolve,reject)=>{
+ setTimeout(() =>{
+   let rollNo =[1,2,3,4];
+   resolve(rollNo);
+   reject('Error while searching');
+ },2000);
+
+});
+
+
+//this is consumng promise
+obj.then((rollNo)=>{
+    console.log(rollNo); // it will give complete list
+console.log(rollNo[2]);// roll no  at 2nd index
+}).catch((error)=> {
+    console.log(error);
+});
 
 //---------------------------------------------------------------------------------------
 
@@ -57,3 +57,19 @@ let p3 = new Promise(function(resolve,reject) {
 Promise.all([p1,p2,p3]).then(function(answer){
 console.log(answer);
 }).catch(error => console.log(`Error in promises ${error}`));
+
+
+// Promise.race method
+    
+// let p1 = Promise.reject(50)
+// let p2 = 250
+// let p3 = new Promise(function(resolve, reject) {
+//     setTimeout(resolve,2000,"Hello")
+//     // setTimeout(reject,1000,"Hello")
+// })
+
+// Promise.race([p1,p2,p3]).then(function(answer){
+//     console.log(answer)
+// }).catch(function(error){
+//     console.log(`error ${error}`)
+// })
