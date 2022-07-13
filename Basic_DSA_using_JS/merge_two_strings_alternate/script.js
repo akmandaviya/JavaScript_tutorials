e30125026ca43ba7bc8259a815ea1059f1873ad7
akmandaviya <abdulmandaviya@gmail.com>
2022-07-13 11:31:27 +0530

    new commit

---
+++ Basic_DSA_using_JS/merge_two_strings_alternate/script.js
@@ -0,0 +1,12 @@
+const a = "abc"
+const b = "def"
+
+const merged = (a,b) =>{
+   let x = ""
+   let size = a.length + b.length
+   for(let i=0; i<size; i++) {
+    if(i < a.length) {
+        x.concat(a.ch)
+    }
+   }
+}
